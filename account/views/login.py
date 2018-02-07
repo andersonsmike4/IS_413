ea4da53ab7c1118dc18e0d8eb27bd36b671dd0c9
andersonsmike4 <andersonsmike4@gmail.com>
2018-02-07 10:42:11 -0700

    added commit and authenticate and login

--- account/views/login.py
+++ account/views/login.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from django_mako_plus import view_function
 from formlib import Formless
 from django import forms
+from account import models as amod
+from django.contrib.auth import authenticate, login
 
 @view_function
 def process_request(request):
@@ -21,7 +23,15 @@ class LoginForm(Formless):
         # create fields
         self.fields['email'] = forms.EmailField(label='Enter Email', required=True)
         self.fields['password'] = forms.CharField(label='Enter Password', widget=forms.PasswordInput(), required=True)
+        self.user = None
 
     def clean(self):
-        email = self.cleaned_data.get('email')
-        password = self.cleaned_data.get('password')
+        self.user = authenticate(email=self.cleaned_data.get('email'), password=self.cleaned_data.get('password'))
+        if self.user is None:
+            raise forms.ValidationError('Invalid email or password.')
+        # return cleaned_data dictionary
+        return self.cleaned_data
+
+    def commit(self):
+        '''Process the form action'''
+        login(self.request, self.user)
