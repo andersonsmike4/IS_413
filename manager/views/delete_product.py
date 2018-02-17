56b89e1e7f3581f655672cb547d15bb916c80cef
andersonsmike4 <andersonsmike4@gmail.com>
2018-02-17 12:02:55 -0700

    added delete link functionality

--- manager/views/delete_product.py
+++ manager/views/delete_product.py
@@ -2,38 +2,11 @@ from django.http import HttpResponseRedirect
 from django_mako_plus import view_function
 from formlib import Formless
 from django import forms
+from catalog import models as cmod
 
 @view_function
-def process_request(request):
+def process_request(request, product: cmod.Product):
 
-    # process the form
-    form = MyForm(request)
-    if form.is_valid():
-        d = form.commit()
-        return HttpResponseRedirect('/app/successurl/')
-
-    # render the template
-    return request.dmp_render('mytemplate.html', {
-        'form': form,
-    })
-
-
-class MyForm(Formless):   # extending formlib.Form, not Django's forms.Form
-    '''An example form'''
-    def init(self):
-        '''Adds the fields for this form (called at end of __init__)'''
-        self.fields['name'] = forms.CharField()
-
-    def clean_name(self):
-        name = self.cleaned_data.get('name')
-        # ...
-        return name
-
-    def commit(self, c):
-        '''Process the form action'''
-        # do something with c (optional)
-        print('>>>>', c)
-        # act on the form
-        print('>>>> Name is', self.cleaned_data['name'])
-        # return any data (optional)
-        return 4
+    product.status = 'I'
+    product.save()
+    return HttpResponseRedirect('/manager/product_list/')
